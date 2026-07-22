d740f1ca86889be336dc05dd26be4ee0a8a53893
Alvin11David <alvin69david@gmail.com>
2026-07-23 00:04:33 +0300

    Auto-commit: 1 file(s) updated at 2026-07-23 00:04:32

--- My-Portfolio-main/portfolio-bfcdcc80-main/src/components/HeroSection.tsx
+++ My-Portfolio-main/portfolio-bfcdcc80-main/src/components/HeroSection.tsx
@@ -176,7 +176,7 @@ const HeroSection = () => {
                 {splitWord(" ", false)}
               </span>
               <span className="text-2xl italic text-green-500 sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl">
-                {splitWord("Digital", true)}
+                {splitWord("Digital", false)}
               </span>
             </span>
             <span
@@ -198,7 +198,7 @@ const HeroSection = () => {
                 {splitWord(" ", false)}
               </span>
               <span className="text-2xl italic text-green-500 sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl">
-                {splitWord("Inspire", true)}
+                {splitWord("Inspire", false)}
               </span>
             </span>
           </h1>

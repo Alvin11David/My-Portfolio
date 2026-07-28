b8240ab84dfbfab0882eb73a7c6e067d6291a1b2
Alvin11David <alvin69david@gmail.com>
2026-07-28 16:07:38 +0300

    Auto-commit: 41 file(s) updated at 2026-07-28 16:07:37

--- My-Portfolio-main/portfolio-bfcdcc80-main/backend/src/main/java/com/portfolio/backend/repository/HeroContentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.portfolio.backend.repository;
-
-import com.portfolio.backend.model.HeroContent;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface HeroContentRepository extends JpaRepository<HeroContent, Long> {
-}

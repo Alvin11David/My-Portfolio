6e037a2d4e0f861dd3d6057d058aaa337f158ea6
Alvin11David <alvin69david@gmail.com>
2026-07-28 15:32:11 +0300

    Auto-commit: 1 file(s) updated at 2026-07-28 15:32:10

--- My-Portfolio-main/portfolio-bfcdcc80-main/backend/mvnw.cmd
+++ My-Portfolio-main/portfolio-bfcdcc80-main/backend/mvnw.cmd
@@ -1,1 +1,11 @@
-@echo off`r`nsetlocal`r`nset MAVEN_WRAPPER_DIR=%~dp0.mvn\wrapper`r`nset MAVEN_WRAPPER_JAR=%MAVEN_WRAPPER_DIR%\maven-wrapper.jar`r`nset MAVEN_WRAPPER_PROPERTIES=%MAVEN_WRAPPER_DIR%\maven-wrapper.properties`r`nset MAVEN_PROJECT_DIR=%~dp0`r`nif not exist "%MAVEN_WRAPPER_JAR%" (`r`n    echo Downloading Maven Wrapper...`r`n)`r`njava -cp "%MAVEN_WRAPPER_JAR%" io.takari.maven.wrapper.MavenWrapperMain --maven-wrapperProperties "%MAVEN_WRAPPER_PROPERTIES%" --maven-projectDir "%MAVEN_PROJECT_DIR%" %*`r`nendlocal
+@echo off
+setlocal
+set MAVEN_WRAPPER_DIR=%~dp0.mvn\wrapper
+set MAVEN_WRAPPER_JAR=%MAVEN_WRAPPER_DIR%\maven-wrapper.jar
+set MAVEN_WRAPPER_PROPERTIES=%MAVEN_WRAPPER_DIR%\maven-wrapper.properties
+set MAVEN_PROJECT_DIR=%~dp0
+if not exist "%MAVEN_WRAPPER_JAR%" (
+    echo Downloading Maven Wrapper...
+)
+java -cp "%MAVEN_WRAPPER_JAR%" io.takari.maven.wrapper.MavenWrapperMain --maven-wrapperProperties "%MAVEN_WRAPPER_PROPERTIES%" --maven-projectDir "%MAVEN_PROJECT_DIR%" %*
+endlocal
